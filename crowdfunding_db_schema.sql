740b5849754b758f6c87520023af568913d864a6
Gloria Gao <141696007+Glowary@users.noreply.github.com>
2023-11-14 23:23:30 -0500

    Update crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,38 +1,38 @@
-CREATE TABLE Category (
-    category_id SERIAL PRIMARY KEY,
+CREATE TABLE Categories (
+    category_id VARCHAR PRIMARY KEY NOT NULL,
     category VARCHAR(100)
 );
 
-CREATE TABLE Subcategory (
-    subcategory_id SERIAL PRIMARY KEY,
+CREATE TABLE Subcategories (
+    subcategory_id VARCHAR PRIMARY KEY NOT NULL,
     subcategory VARCHAR(100)
 );
 
-CREATE TABLE Contact (
-    contact_id SERIAL PRIMARY KEY,
+CREATE TABLE Contacts (
+    contact_id VARCHAR(100) PRIMARY KEY NOT NULL,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     email VARCHAR(100)
 );
 
-CREATE TABLE Campaign (
-    cf_id SERIAL PRIMARY KEY,
-    company_name VARCHAR(100),
+CREATE TABLE Campaigns (
+    cf_id INT PRIMARY KEY NOT NULL,
+    contact_id INT REFERENCES Contact(contact_id),
+    company_name VARCHAR(255),
     description TEXT,
     goal NUMERIC,
     pledged NUMERIC,
-    outcome VARCHAR(100),
+    outcome VARCHAR(50),
     backers_count INT,
     country VARCHAR(100),
-    currency VARCHAR(100),
+    currency VARCHAR(50),
     launch_date DATE,
     end_date DATE,
-	contact_id INT REFERENCES Contact(contact_id),
-    category_id VARCHAR REFERENCES Category(category_id),
-    subcategory_id VARCHAR REFERENCES Subcategory(subcategory_id)
+    category_id INT REFERENCES Category(category_id),
+    subcategory_id INT REFERENCES Subcategory(subcategory_id)
 );
 
+SELECT * FROM Category;
 SELECT * FROM Subcategory;
 SELECT * FROM Contact;
-SELECT * FROM Category;
 SELECT * FROM Campaign;
