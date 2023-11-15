ae5950e98128014dbe772bca8e95f790d6be2a40
Gloria Gao <141696007+Glowary@users.noreply.github.com>
2023-11-14 23:52:16 -0500

    Update crowdfunding_db_schema.sql

--- crowdfunding_db_schema.sql
+++ crowdfunding_db_schema.sql
@@ -1,23 +1,24 @@
-CREATE TABLE Categories (
+CREATE TABLE Category(
     category_id VARCHAR PRIMARY KEY NOT NULL,
     category VARCHAR(100)
 );
 
-CREATE TABLE Subcategories (
+CREATE TABLE Subcategory(
     subcategory_id VARCHAR PRIMARY KEY NOT NULL,
     subcategory VARCHAR(100)
 );
 
-CREATE TABLE Contacts (
+CREATE TABLE Contact(
     contact_id VARCHAR(100) PRIMARY KEY NOT NULL,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
     email VARCHAR(100)
 );
 
-CREATE TABLE Campaigns (
+CREATE TABLE Campaign(
     cf_id INT PRIMARY KEY NOT NULL,
-    contact_id INT REFERENCES Contact(contact_id),
+    contact_id varchar(200) NOT NULL,
+		FOREIGN KEY (category_id) REFERENCES Contact(contact_id),
     company_name VARCHAR(255),
     description TEXT,
     goal NUMERIC,
@@ -28,8 +29,10 @@ CREATE TABLE Campaigns (
     currency VARCHAR(50),
     launch_date DATE,
     end_date DATE,
-    category_id INT REFERENCES Category(category_id),
-    subcategory_id INT REFERENCES Subcategory(subcategory_id)
+    category_id varchar(200) NOT NULL,
+		FOREIGN KEY (category_id) REFERENCES Category(category_id),
+    subcategory_id varchar(200) NOT NULL,
+		FOREIGN KEY (subcategory_id) REFERENCES Subcategory(subcategory_id)
 );
 
 SELECT * FROM Category;
